edd1c058fdfb0aa29162f8c5779504e2568837e0
chrischriscris <18-10892@usb.ve>
2020-12-26 01:45:07 -0400

    optimized command line version

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 from cromatic import  square_collage, bar_mapping
+import os, sys
 
-DIR = '/home/leochris/Instagram/chrischriscris/'
+f = lambda path: int(len(os.listdir(path))**0.5)
+
+user = sys.argv[1]
+
+DIR = f'/home/leochris/Instagram/{user}/'
 TEMP_DIR = '/home/leochris/Documentos/Programación/Python/collage/temp/'
 BAR = '/home/leochris/Documentos/Programación/Python/collage/croma.jpeg'
+
+size = f(DIR)
 
 # Returns rainbow square collage, takes name of the output and 
 # size of the collage
@@ -11,4 +18,4 @@ def main(n, output="collage", spp=100):
 	cover_list = bar_mapping(DIR, TEMP_DIR, BAR, n)
 	square_collage(cover_list, DIR, n, output, spp)
 
-main(7, "chus", 285)
+main(size, f'{user}_collage', 2000//size)
